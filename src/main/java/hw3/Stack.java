2df2bd2d4c54eaa89dcc20662fb4be53a0151846
nmusi <n.musinov@gmail.com>
2022-04-05 09:46:15 +0300

    hw3

---
+++ src/main/java/hw3/Stack.java
@@ -0,0 +1,19 @@
+package hw3;
+
+public interface Stack<E> {
+
+    boolean push(E value);
+
+    E pop();
+
+    E peek(); //top()
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
